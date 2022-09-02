b6cacfa455f247f474c28bffd46da7ff8f6d51c8
roddlek <kyungbread@gmail.com>
2022-09-02 20:52:05 +0900

    filter js-4

--- src/page/Section01.js
+++ src/page/Section01.js
@@ -2,23 +2,26 @@ import Data from './data.json';
 import './section.css'
 import { Link } from "react-router-dom";
 import { useState } from 'react';
-import List from './components/cardList'
+//import List from './components/cardList'
 
 function Section01(){
-    //데이터 설정
-    const items = Data.sec01;
-    const selectList = ["Select", "Ring", "Earrings", "Bangle"]
-    //useState
-    //const [activeSelc, setActiveSelc] = useState(selectList);
-    const [data, setData] = useState(items);
-    //fillter
-    const activeSelecList = (list) => {
-        if(list === "Select"){
-            setData(items);
+    const [data, setData] = useState(Data.sec01);
+    const [filterData, setFilterData] = useState(Data.sec01)
+
+    function changeFilter(e){
+        //console.log(e.target.value)
+        //changeList(e.target.value);
+        if(e.target.value === "Select"){
+            setData(data);
             return;
         }
-        const filterList = items.filter((item) => list === items.value);
-        setData(filterList);
+        let filterList = Data.sec01.filter( (item) => e.target.value === item.value );
+        setFilterData(filterList);
+    }
+
+    function changeList(){
+        setFilterData.map((item) => console.log(item));
+        
     }
 
     return(
@@ -31,26 +34,27 @@ function Section01(){
                 <li><span>&gt;</span><Link to="/Section01">Collections</Link></li>
             </ul>
             <div>
-                <ul>
-                    {selectList.map((btn, idx) => {
-                        return(
-                            <li
-                                key={idx}
-                                onClick={() => activeSelecList(btn)}
-                            >
-                            {btn}
-                            </li>
-                        )
-                    })}
-                </ul>
+                <select
+                    onChange={changeFilter}
+                >
+                    <option value="Select">Select</option>
+                    <option value="Ring">Ring</option>
+                    <option value="Earrings">Earrings</option>
+                    <option value="Bangle">Bangle</option>
+                </select>
             </div>
             <section className='page mgb'>
-                {data.map( (a, idx) => {
-                    return(
-                        <List card={a} key={idx} />
-                    )
-                }
-                )}
+                {changeList()}
+                {/* <figure>
+                    <img src={process.env.PUBLIC_URL + img} alt={title} />
+                    <figcaption>
+                        <dl>
+                            <dt>title</dt>
+                            <dd>content</dd>
+                            <dd>price</dd>
+                        </dl>
+                    </figcaption>
+                </figure> */}
             </section>
         </article>
         </>
